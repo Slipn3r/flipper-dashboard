8fcf425a87669b49d3e180101d52080d43915f26
Maxim Lyubimov <m.lyubimov@flipperdevices.com>
2024-04-03 17:46:55 +0400

    feat: Split logic for Archive Page

---
+++ frontend/src/stores/pages/Archive/store-web.js
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia'
+import { computed } from 'vue'
+
+import { log } from 'composables/useLog'
+
+import { useMainStore } from 'stores/global/main'
+const mainStore = useMainStore()
+
+export const useArchiveWebStore = (ArchiveMainStore) => {
+  return defineStore('ArchiveWeb', () => {
+    const mainFlags = computed(() => mainStore.flags)
+
+    const startRpc = async () => {
+      mainFlags.value.rpcToggling = true
+      await ArchiveMainStore.flipper.value.startRPCSession()
+        .then(() => {
+          mainFlags.value.rpcActive = true
+          mainStore.setRpcStatus(true)
+          mainFlags.value.rpcToggling = false
+          log({
+            level: 'info',
+            message: `${ArchiveMainStore.componentName}: RPC started`
+          })
+        })
+        .catch(error => {
+          console.error(error)
+          log({
+            level: 'error',
+            message: `${ArchiveMainStore.componentName}: Error while starting RPC: ${error.toString()}`
+          })
+        })
+    }
+
+    const start = async () => {
+      if (!mainFlags.value.rpcActive) {
+        await startRpc()
+      }
+      await ArchiveMainStore.list()
+    }
+
+    return { start }
+  })()
+}
