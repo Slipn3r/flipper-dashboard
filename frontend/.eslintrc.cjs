f46065619cd5cbd0cce8ef1949966e25b719344a
Maxim Lyubimov <m.lyubimov@flipperdevices.com>
2024-09-14 11:04:16 +0400

    feat: Add a separate class for rendering frames

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -102,7 +102,9 @@ module.exports = {
     'prefer-promise-reject-errors': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    '@typescript-eslint/no-non-null-assertion': 'off'
   },
 
   globals: {
